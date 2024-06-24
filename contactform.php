c987500b39b01dc53f8663109c0cf38693b4f0ad
sonal <sonaldilmith15@gmail.com>
2024-06-24 20:37:04 +0530

    first commit

---
+++ contactform.php
@@ -0,0 +1,40 @@
+<?php 
+
+$name               = $_POST['name'];
+$email              = $_POST['email'];
+$subject            = $_POST['subject'];
+$message            = $_POST['message'];
+
+
+//create connection
+
+$conn=new mysqli('localhost','root','','tailwaggers');
+
+if (isset($_POST['submit'])) {
+    $sql = "INSERT INTO contactus (
+    name,
+    email,
+    subject,
+    message
+    )
+VALUES(
+    '$name',
+    '$email',           
+    '$subject',
+    '$message'
+
+)";
+
+
+
+
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+        header("refresh:3;url=index.php"); //this line used to redirect to the index.php page after 3 seconds
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
+?>
